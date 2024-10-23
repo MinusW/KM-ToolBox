5e5e33226bcb8861254f527c31ef86a16272c5a5
Minus <61775646+ShansheGundishvili@users.noreply.github.com>
2024-10-23 18:50:25 +0200

    fix: trait counts

--- noble.rb
+++ noble.rb
@@ -2,7 +2,8 @@ require 'csv'
 
 class Noble
   attr_accessor :role, :level, :tier, :type, :traits
-  def initialize(role, level, tier = "legendary")
+
+  def initialize(role, level = 1, tier = 'legendary')
     @role = role
     @level = level
     @tier = tier
@@ -12,37 +13,41 @@ class Noble
 
   def score
     return 0 if @traits.empty?
+
     @traits.sum { |trait| trait.score(self) }
   end
 
   def add_trait(trait_name, level)
-  trait_to_add = Defaults.instance.traits.find { |trait| trait.name == trait_name && trait.level == level }
+    trait_to_add = Defaults.instance.traits.find { |trait| trait.name == trait_name && trait.level == level }
 
-  @traits << trait_to_add if trait_to_add
+    @traits << trait_to_add if trait_to_add
   end
 
-
   def potential_traits
-    (Defaults.instance.traits - @traits).sort_by { |trait| [-trait.score(self), trait.name] }.first(max_innate_count - innates_trait_count)
+    (Defaults.instance.traits - @traits).sort_by do |trait|
+      [-trait.score(self), trait.name]
+    end.first(max_innate_count - innates_trait_count)
   end
 
   def potential_score
-    @traits += potential_traits
+    potential = potential_traits
+    @traits += potential
     total_score = score
-    @traits -= potential_traits
+    @traits -= potential
     total_score
   end
-
 
   def innates_trait_count
     total = 0
     total = Defaults.instance.traits_on_tier[@tier]
-
-    total += (@level/Defaults.instance.levels_per_stage).ceil-1
+    total += ((@level - 1) / Defaults.instance.levels_per_stage).ceil - 1
+    total
   end
 
   def max_innate_count
-    Defaults.instance.stages[@role] - 1 + Defaults.instance.traits_on_tier[@tier]
+    return unless @tier == 'legendary'
+
+    Defaults.instance.stages[@role] - 1 + Defaults.instance.traits_on_tier[@tier] + Defaults.instance.traits_on_tier['mythical'] - Defaults.instance.traits_on_tier[@tier]
   end
 
   def delete_traits_with_name(trait_name)
