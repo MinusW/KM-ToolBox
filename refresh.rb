5111f3f54d567b9918857dc44ca6c415f3d6743f
Minus <61775646+ShansheGundishvili@users.noreply.github.com>
2024-10-23 18:49:34 +0200

    refactor: change to 5 and formatting

--- refresh.rb
+++ refresh.rb
@@ -1,8 +1,9 @@
 class Refresh
   @@timeset = 3600
-  @@user_limit = 3
+  @@user_limit = 5
   class RefreshLog
     attr_reader :user, :channel, :timestamp
+
     def initialize(user, channel)
       @user = user
       @channel = channel
@@ -16,9 +17,7 @@ class Refresh
 
   def add_log(user, channel)
     existing_log = @logs.last(5).find { |log| log.user == user }
-    if existing_log
-      @logs.delete(existing_log)
-    end
+    @logs.delete(existing_log) if existing_log
     @logs << RefreshLog.new(user, channel)
   end
 
@@ -28,6 +27,7 @@ class Refresh
 
   def refresh?
     return false if @logs.size < @@user_limit
+
     @logs.last(@@user_limit).all? { |log| log.timestamp > Time.now - @@timeset }
   end
 
