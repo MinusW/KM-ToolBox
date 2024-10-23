7d6fb80a1d505759e91bb960d644935d20e6016c
Minus <61775646+ShansheGundishvili@users.noreply.github.com>
2024-10-23 19:11:17 +0200

    refactor: proper indenting and reformat

--- group.rb
+++ group.rb
@@ -1,23 +1,26 @@
 class Group
-    attr_accessor :name, :channel, members
-    def initialize(name, channel)
-        @name = name
-        @channel = channel
-        @members = []
-    end
+  attr_accessor :name, :channel, members
+  def initialize(name, channel)
+    @name = name
+    @channel = channel
+    @members = []
+  end
 
-    def add_member(member)
-        return if @members.include?(member)
-        @channel.add_group_users(member.id)
-    end
+  def add_member(member)
+    return if @members.include?(member)
 
-    def remove_member(member)
-        return if !@members.include?(member)
-        @channel.remove_group_users(member.id)
-    end
+    @channel.add_group_users(member.id)
+  end
 
-    def delete(member)
-        return if !@members.include?(member)
-        @channel.delete
-    end
+  def remove_member(member)
+    return unless @members.include?(member)
+
+    @channel.remove_group_users(member.id)
+  end
+
+  def delete(member)
+    return unless @members.include?(member)
+
+    @channel.delete
+  end
 end
