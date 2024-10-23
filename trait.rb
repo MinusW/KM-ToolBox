38f6c3426c2bd2ab6c1c78d2880b2aa57a10a2e5
Minus <61775646+ShansheGundishvili@users.noreply.github.com>
2024-10-23 18:47:53 +0200

    refactor: format Trait

--- trait.rb
+++ trait.rb
@@ -1,14 +1,14 @@
 class Trait
-    attr_accessor :name, :level, :buffs, :type
-    
-    def initialize(name, level, type)
-      @name = name
-      @level = level
-      @type = type
-      @buffs = []
-    end
-    
-    def score(noble)
-      @buffs.sum { |buff| buff.score(noble) }
-    end
+  attr_accessor :name, :level, :buffs, :type
+
+  def initialize(name, level, type)
+    @name = name
+    @level = level
+    @type = type
+    @buffs = []
+  end
+
+  def score(noble)
+    @buffs.sum { |buff| buff.score(noble) }
+  end
 end
