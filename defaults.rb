a22867364e55ab233b7e0b61bb6f1ba8e4da1194
Minus <61775646+ShansheGundishvili@users.noreply.github.com>
2024-10-23 19:12:27 +0200

    fix: levels per stage and interactions

--- defaults.rb
+++ defaults.rb
@@ -9,16 +9,6 @@ class Defaults
   include Singleton
 
   def initialize
-    @multipliers = {}
-    @traits = []
-    @stages = {}
-    @traits_on_tier = {}
-    @levels_per_stage = 40
-    @buildings = {}
-    @noble_types = {}
-    @interactions = {}
-    @emojis = {}
-
     initialize_defaults
   end
 
@@ -28,10 +18,11 @@ class Defaults
     @stages = set_stages('data/stages.csv')
     @traits_on_tier = set_traits_on_tier('data/traitsOnTier.csv')
     @buildings = set_buildings('data/buildings/')
-    @noble_types = set_noble_types()
-    @emojis = set_emojis()
+    @noble_types = set_noble_types
+    @emojis = set_emojis
     @refresh_logs = Refresh.new
     @noble_interactions = NobleInteractions.new
+    @levels_per_stage = 40
   end
 
   def set_multipliers(csv_file)
@@ -49,18 +40,19 @@ class Defaults
 
   def set_traits(csv_file)
     traits = []
-  
+
     CSV.foreach(csv_file, headers: true, col_sep: ';') do |row|
-      trait = Trait.new(row["trait"], row["level"].to_i, row["type"])
+      trait = Trait.new(row['trait'], row['level'].to_i, row['type'])
       if row.headers && row.headers.size >= 5
-        row.headers[4..-1].each do |buff_name|
-          next if row[buff_name] == '0'
+        row.headers[3..-1].each do |buff_name|
+          next unless row[buff_name]
+
           trait.buffs << Buff.new(buff_name, row[buff_name].to_i)
         end
       end
       traits << trait
     end
-    
+
     traits
   end
 
@@ -68,7 +60,7 @@ class Defaults
     stages = {}
 
     CSV.foreach(csv_file, headers: true, col_sep: ';') do |row|
-      stages[row["role"]] = row["stages"].to_i
+      stages[row['role']] = row['stages'].to_i
     end
     stages
   end
@@ -77,17 +69,18 @@ class Defaults
     traits_on_tier = {}
 
     CSV.foreach(csv_file, headers: true, col_sep: ';') do |row|
-      traits_on_tier[row["tier"]] = row["traitcount"].to_i
+      traits_on_tier[row['tier']] = row['traitcount'].to_i
     end
     traits_on_tier
   end
 
   def set_building(csv_file)
     levels = {}
-    building_name = File.basename(csv_file, ".csv")
+    building_name = File.basename(csv_file, '.csv')
 
     CSV.foreach(csv_file, headers: true, col_sep: ';').with_index(1) do |row, index|
-      levels[index] = BuildingLevel.new(row["keepLevel"], row["common"], row["uncommon"], row["metalType"], row["stone"], row["count"])
+      levels[index] =
+        BuildingLevel.new(row['keepLevel'], row['common'], row['uncommon'], row['metalType'], row['stone'], row['count'])
     end
 
     Building.new(building_name, levels)
@@ -97,28 +90,29 @@ class Defaults
     buildings = {}
     Dir.foreach(directory) do |file|
       next if file == '.' or file == '..' or File.extname(file) != '.csv'
-      buildings[File.basename(file, ".csv")] = set_building(directory + file)
+
+      buildings[File.basename(file, '.csv')] = set_building(directory + file)
     end
     buildings
   end
 
   def set_noble_types
-    return {
-      "captain" => 'combat',
-      "knight" => 'combat',
-      "subjugator" => 'combat',
-      "explorer" => 'exploration',
-      "merchant" => 'trading',
-      "collier" => 'mining'
+    {
+      'captain' => 'combat',
+      'knight' => 'combat',
+      'subjugator' => 'combat',
+      'explorer' => 'exploration',
+      'merchant' => 'trading',
+      'collier' => 'mining'
     }
   end
 
   def set_emojis
-    return {
-      "1" => "1️⃣",
-      "2" => "2️⃣",
-      "3" => "3️⃣",
-      "4" => "4️⃣",
+    {
+      '1' => '1️⃣',
+      '2' => '2️⃣',
+      '3' => '3️⃣',
+      '4' => '4️⃣'
     }
   end
 
