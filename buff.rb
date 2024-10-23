56f15ac7d31cee7c2f2e3bc795f6f32720487191
Minus <61775646+ShansheGundishvili@users.noreply.github.com>
2024-10-23 19:14:01 +0200

    refactor: format

--- buff.rb
+++ buff.rb
@@ -1,12 +1,12 @@
 class Buff
-    attr_accessor :name, :value
+  attr_accessor :name, :value
 
-    def initialize(name, value, multiplier = 1)
-        @name = name
-        @value = value
-    end
+  def initialize(name, value, multiplier = 1)
+    @name = name
+    @value = value
+  end
 
-    def score(noble)
-      @value*Defaults.instance.multipliers[noble.role][@name]
-    end
+  def score(noble)
+    @value * Defaults.instance.multipliers[noble.role][@name]
+  end
 end
